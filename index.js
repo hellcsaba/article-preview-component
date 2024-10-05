7522157086b5ac18e5625d59ec055e15ee1766c3
Csaba Hell <ifj.hellcsaba@gmail.com>
2024-10-05 23:01:47 +0200

    Add logic for showing share-content section on both mobile and desktop view

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const articleShareButtonInv = document.querySelector(
 );
 
 articleShareButton.addEventListener("click", () => {
+  const inverseButtonStyle = window.getComputedStyle(articleShareButtonInv);
+  if (inverseButtonStyle.display !== "none") {
+    articleAuthorContent.classList.toggle("hidden");
+  }
   articleShareContent.classList.toggle("hidden");
-  articleAuthorContent.classList.toggle("hidden");
 });
 
 articleShareButtonInv.addEventListener("click", () => {
   articleShareContent.classList.toggle("hidden");
-  articleAuthorContent.classList.toggle("hidden");
+  articleAuthorContent.classList.remove("hidden");
 });
